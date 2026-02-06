13867d8315d30d4315ac46005f0d25bf379f4659
Juhwan21st <jseo9123@conestogac.on.ca>
2026-02-06 13:40:22 -0500

    Apply initial migration and generate SQLite database

---
+++ Migrations/20260208040025_Init.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Assignment1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CourseCode = table.Column<string>(type: "TEXT", nullable: false),
+                    CourseName = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Credits = table.Column<int>(type: "INTEGER", nullable: false),
+                    DeliveryMode = table.Column<int>(type: "INTEGER", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Courses");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Assignment 1: Building a Functional Microservice with CRUD and Documentation
+
+Course: PROG3176 - Winter 2026 - Section 1  
+Programmed by: Juhwan Seo [8819123]
